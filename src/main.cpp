3e79fe4773a925265525ef557e32d4be89571393
Divesh Ogha <diveshogha@Diveshs-MacBook-Air.local>
2025-12-03 17:13:02 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -51,13 +51,16 @@ int main(int argc, char **argv) {
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment the code below to pass the first stage
-  
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  // 
+  // accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  // std::cout << "Client connected\n";
+  // 
+  // close(server_fd);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   const char *response = "+PONG\r\n";
   send(client_fd, response, strlen(response), 0);
-  close(client_fd); 
+  close(client_fd);
   close(server_fd);
-
   return 0;
 }
